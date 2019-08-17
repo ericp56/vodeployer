5101f9d43f767cb13c3db911b004ccb37c17b23b
ericp56 <ericp@neso.localdomain>
2019-08-17 17:23:52 -0400

    Organized for maven

---
+++ VODeployer/src/main/java/com/ivs/App.java
@@ -0,0 +1,13 @@
+package com.ivs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
